62f760ab4035d1d17b22e392572ad1057f5099f2
MohammadAbdulRihan <abdulrihan897@gmail.com>
2025-07-31 14:54:24 +0530

    feat: implement user-specific issue counts and latest issues retrieval in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,23 @@
 import prisma from '@/prisma/client';
 import DashboardPageClient from './page.client';
 import { Metadata } from 'next';
-
+import { getServerSession } from 'next-auth';
+import authOptions from '../auth/authOptions';
 export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);  
+  //@ts-ignore
+  const user = session?.user as typeof session.user & { id: string };
   const open = await prisma.issue.count({
-    where: { status: 'OPEN' },
+    where: { status: 'OPEN',userId:user.id },
   });
   const inProgress = await prisma.issue.count({
-    where: { status: 'IN_PROGRESS' },
+    where: { status: 'IN_PROGRESS',userId:user.id },
   });
   const closed = await prisma.issue.count({
-    where: { status: 'CLOSED' },
+    where: { status: 'CLOSED',userId:user.id },
   });
   const latestIssues = await prisma.issue.findMany({
+    where: { userId: user.id },
     orderBy: { createdAt: 'desc' },
     take: 5,
     include: { assignedToUser: true },
